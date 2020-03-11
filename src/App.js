c2db14dbc11d6a0d76f83fbec3c6aebbe8b357cb
Brennan Newton <brennanmnewton@gmail.com>
2020-03-11 15:54:57 -0700

    Style landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // Components
 import React from 'react';
 import SocialBar from './js/SocialBar';
+import Logo from './img/bnd_logo_transparent.png';
+import DownArrow from './img/down_arrow.png';
 
 // Styles
 import './App.css';
@@ -10,6 +12,9 @@ function App() {
   return (
     <div className="App">
       <SocialBar/>
+      <img src={Logo} alt="Brennan Newton Designs logo" className="Logo"/>
+      <br/>
+      <img src={DownArrow} alt="Downward arrow" className="DownArrow m-3"/>
     </div>
   );
 }
