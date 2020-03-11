6ee5b1e11da3f616f8b80f2ad95bbe2060e8c240
Brennan Newton <brennanmnewton@gmail.com>
2020-03-11 16:01:33 -0700

    Add build directory for deployment

---
+++ build/precache-manifest.6230321d075d1d4da585cfa06e6a0b18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857c34b67716bb5bff40dfe2f4ff498e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "658c17b4ddb795fc3a3a",
+    "url": "/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "380a86fcf38b237bd49a",
+    "url": "/static/css/main.76a83d03.chunk.css"
+  },
+  {
+    "revision": "658c17b4ddb795fc3a3a",
+    "url": "/static/js/2.1fa80e05.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1fa80e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380a86fcf38b237bd49a",
+    "url": "/static/js/main.0690facb.chunk.js"
+  },
+  {
+    "revision": "03b2621959adffdfedef",
+    "url": "/static/js/runtime-main.ffe2e9c5.js"
+  },
+  {
+    "revision": "31c18d03d7ad8608bb7d6ccc85682508",
+    "url": "/static/media/bnd_logo_transparent.31c18d03.png"
+  }
+]);
