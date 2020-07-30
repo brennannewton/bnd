8ea5ffe75f75414f5383131456dfb49b941a0101
Brennan Newton <brennanmnewton@gmail.com>
2020-07-30 00:57:31 -0400

    Change Footer class to footerStyle state for dynamic styling

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -5,10 +5,17 @@ import React from 'react';
 import '../css/Footer.css';
 
 export default class Footer extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      footerStyle: "",
+    };
+  }
+
   render() {
     return (
-      <div className="Footer w-100">
-        <h1>thebndesigns@gmail.com</h1>
+      <div className={`${this.state.footerStyle} w-100`}>
+        <h1 id="Email">thebndesigns@gmail.com</h1>
       </div>
     )
   }
